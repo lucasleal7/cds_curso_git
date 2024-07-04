f864783bc1dc1bb70431844848243112dd990282
Lucas Leal <leal13henriquevida@gmail.com>
2024-07-04 16:26:31 -0300

    finalizado projeto

--- src/extraction.py
+++ src/extraction.py
@@ -1,4 +1,5 @@
-import pandas as pd 
+import pandas as pd
+
 
 def load_data():
-    return pd.read_csv(r"C:\Users\55149\OneDrive - Fatec Centro Paula Souza\Documents\repos\curso_git\projeto\data\raw\bike.csv")
+    return pd.read_csv("data/bikes_completed.csv")
